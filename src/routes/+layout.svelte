3e95337cdf1c45ec814c867eec9da0ea31f57483
rizal suryawan <rizalsuryawan86@gmail.com>
2024-05-20 11:42:55 +0700

    feat: initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import '../app.css'
+	import { page } from '$app/stores'
+</script>
+
+<main class="container">
+	<nav>
+		<ul>
+			<li>
+				<a href="/"> home</a>
+			</li>
+			<li>
+				<a href="/posts"> posts</a>
+			</li>
+			<li>
+				<a href="/{$page.params.slug}">{$page.params.slug}</a>
+			</li>
+		</ul>
+	</nav>
+	<slot />
+</main>
---
+++ src/routes/[slug]/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { page } from '$app/stores'
+</script>
+
+<h1>{$page.params.slug}</h1>
+<p>{$page.url}</p>
+
+<pre>
+  {JSON.stringify($page, null, 2)}
+</pre>
+
+<style>
+	h1 {
+		color: red;
+		/* text-transform: uppercase; */
+	}
+</style>
